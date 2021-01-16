839ebb93947021a098583f04c30a98b22c4ad4d8
Christian Kischy Haege <kischy@gmx.de>
2021-01-16 16:36:53 +0100

    Added fibonacci tests

---
+++ p25-euler-tests/TestFibonacci.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using p25_euler;
+
+namespace p25_euler_tests
+{
+    [TestClass]
+    public class TestFibonacci
+    {
+        Fibonacci fibonacci = new Fibonacci();
+
+        [TestMethod]
+        public void TestFibonacciCalculationFirst12()
+        {
+            Assert.AreEqual(2, fibonacci.getNextFibonacci());
+            Assert.AreEqual(3, fibonacci.Index);
+
+            Assert.AreEqual(3, fibonacci.getNextFibonacci());
+            Assert.AreEqual(4, fibonacci.Index);
+
+            Assert.AreEqual(5, fibonacci.getNextFibonacci());
+            Assert.AreEqual(5, fibonacci.Index);
+
+            Assert.AreEqual(8, fibonacci.getNextFibonacci());
+            Assert.AreEqual(6, fibonacci.Index);
+
+            Assert.AreEqual(13, fibonacci.getNextFibonacci());
+            Assert.AreEqual(7, fibonacci.Index);
+
+            Assert.AreEqual(21, fibonacci.getNextFibonacci());
+            Assert.AreEqual(8, fibonacci.Index);
+
+            Assert.AreEqual(34, fibonacci.getNextFibonacci());
+            Assert.AreEqual(9, fibonacci.Index);
+
+            Assert.AreEqual(55, fibonacci.getNextFibonacci());
+            Assert.AreEqual(10, fibonacci.Index);
+
+            Assert.AreEqual(89, fibonacci.getNextFibonacci());
+            Assert.AreEqual(11, fibonacci.Index);
+
+            Assert.AreEqual(144, fibonacci.getNextFibonacci());
+            Assert.AreEqual(12, fibonacci.Index);
+
+        }
+    }
+}
---
+++ p25-euler/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace p25_euler
+{
+    public class Fibonacci
+    {
+        private BigInteger Fn_m1;
+        private BigInteger Fn_m2;
+
+
+        public int Index
+        {
+            get;
+        }
+
+        public Fibonacci()
+        {
+            Fn_m1 = 1;
+            Fn_m2 = 1;
+            Index = 2;
+        }
+
+        public BigInteger getNextFibonacci()
+        {
+            return 0;
+        }
+
+
+    }
+}
