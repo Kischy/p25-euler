506c81cdad73e2fda81de6916af737e0c2874bea
Christian Kischy Haege <kischy@gmx.de>
2021-01-16 16:41:58 +0100

    Added callculation of first fibonacci with 1000 digits

--- p25-euler/p25_euler.cs
+++ p25-euler/p25_euler.cs
@@ -9,7 +9,14 @@ namespace p25_euler
             Console.WriteLine("Calculation started");
 
 
-            int answer_p25 = 0;
+            Fibonacci fibonacci = new Fibonacci();
+
+
+            while(DigitCounter.GetNumberOfDigits(fibonacci.GetNextFibonacci()) != 1000)
+            {
+            }
+
+            int answer_p25 = fibonacci.Index;
 
             Console.WriteLine("The answer to problem 24 of project Euler is " + answer_p25 + ".");
         }
